ab496e15f47ecf17c524e0f33c1ce6d5358bbb01
Gokulprasad Thangavel <gokulprasad.t@appviewx.com>
2023-12-05 21:52:22 +0530

    Deep dive on bundlers

---
+++ lesson-2/App.js
@@ -0,0 +1,20 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+const parent = React.createElement("div", { id: "parent" }, [
+  React.createElement("div", { id: "child" }, [
+    React.createElement("h1", {}, "I'm h1 tag"),
+    React.createElement("h2", {}, "I'm h2 tag"),
+  ]),
+  React.createElement("div", { id: "child2" }, [
+    React.createElement("h1", {}, "I'm h1 tag"),
+    React.createElement("h2", {}, "I'm h2 tag"),
+  ]),
+]);
+
+// JSX
+
+console.log(parent); // object
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(parent);
